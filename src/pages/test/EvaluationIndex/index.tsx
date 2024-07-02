5ec0757937bacd41787b15e18ca4ce53d54de36c
Yalin814 <13203701351@163.com>
2024-07-02 16:13:06 +0800

    fix: 驾驶舱-样式调整-避免label被marker遮挡

--- src/pages/test/EvaluationIndex/index.tsx
+++ src/pages/test/EvaluationIndex/index.tsx
@@ -5,7 +5,7 @@ import { useSearchParams } from 'react-router-dom'
 import { GetRateByDeptsReq, LoadDepartmentChildrenResp } from '@/api/ccms/evaluate/types'
 import { Button, Form, Row, Col, DatePicker, FormProps, Spin, message } from 'antd'
 import TrendChart from './components/TrendChart'
-import { FileSearchOutlined } from '@ant-design/icons'
+import { FileSearchOutlined, LeftOutlined } from '@ant-design/icons'
 import dayjs from 'dayjs'
 import red from '@/assets/images/point10.png'
 import green from '@/assets/images/point100.png'
@@ -240,10 +240,11 @@ const MapTest = () => {
       <Spin spinning={spinning} tip="Loading...">
         <div className="map" ref={mapContainerRef}>
           <Button onClick={goBack} className="go-back">
-            <img
+            {/* <img
               style={{ width: 14 }}
               src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M448 440a16 16 0 01-12.61-6.15c-22.86-29.27-44.07-51.86-73.32-67C335 352.88 301 345.59 256 344.23V424a16 16 0 01-27 11.57l-176-168a16 16 0 010-23.14l176-168A16 16 0 01256 88v80.36c74.14 3.41 129.38 30.91 164.35 81.87C449.32 292.44 464 350.9 464 424a16 16 0 01-16 16z'/></svg>"
-            />
+            /> */}
+            <LeftOutlined />
             上级部门
           </Button>
         </div>
