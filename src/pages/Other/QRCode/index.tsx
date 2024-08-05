58a9207043b41f783249d51ecda73896299e21cb
Yalin814 <13203701351@163.com>
2024-08-05 17:11:33 +0800

    test: qrcode.react

--- src/pages/Other/QRCode/index.tsx
+++ src/pages/Other/QRCode/index.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from 'react'
 import { QRCode } from 'antd'
+import { QRCodeCanvas } from 'qrcode.react'
 
 const QRCodeTest = () => {
   useEffect(() => {}, [])
 
   return (
     <>
-      <div id="qrcode"></div>
-      <QRCode value="https://ant-design.antgroup.com/components/qr-code-cn?from=msidevs.net" />
+      <div title="Ant Design QR Code">
+        <QRCode value="https://ant-design.antgroup.com/components/qr-code-cn?from=msidevs.net" />
+      </div>
+      <div title="qrcode.react">
+        <QRCodeCanvas value="https://www.npmjs.com/package/qrcode.react" />
+      </div>
     </>
   )
 }
